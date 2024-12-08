860e7bc4a5d867f118a400f733639d9c91784502
Alexander G. Morano <amorano@gmail.com>
2024-12-08 15:45:54 -0500

    Fix build configuration for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '',
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    basePath: '',
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+    assetPrefix: '/',
 };
 
 export default nextConfig;
